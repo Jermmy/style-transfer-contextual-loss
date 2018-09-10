b273703aec30115effdbf90e5eec87609e66aa6e
Jermmy <503262037@qq.com>
2018-09-10 20:08:12 +0800

    update generator

--- model/vgg.py
+++ model/vgg.py
@@ -12,36 +12,39 @@ def conv(in_channels, out_channels):
     return nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
 
 
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
 class VGG19(nn.Module):
 
     def __init__(self):
         super(VGG19, self).__init__()
         self.conv3_2 = nn.Sequential(
             conv(3, 64),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             conv(64, 64),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
             conv(64, 128),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             conv(128, 128),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
             conv(128, 256),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             conv(256, 256),
-            nn.ReLU(inplace=True)
+            nn.ReLU()
         )
         self.conv4_2 = nn.Sequential(
             conv(256, 256),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             conv(256, 256),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
             conv(256, 512),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             conv(512, 512),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
         )
 
     def forward(self, x):
--- train.py
+++ train.py
@@ -8,9 +8,25 @@ import torchvision.transforms as transforms
 import numpy as np
 import argparse
 import collections
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import os
 from os.path import exists, join
+
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+
+def tensor2image(T):
+    image = T.detach().cpu().numpy()[0].transpose((1, 2, 0))
+    for i in range(len(mean)):
+        image[:, :, i] = (image[:, :, i] * std[i]) + mean[i]
+    image = image * 255.
+    image = np.minimum(np.maximum(image, 0.0), 255.0)
+
+    return image.astype(np.uint8)
+
 
 def main(config):
 
@@ -18,9 +34,11 @@ def main(config):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    normalize = transforms.Normalize(mean=mean, std=std)
     transform = transforms.Compose([
         transforms.Resize((config.image_size, config.image_size)),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        normalize
     ])
 
     train_dataset = TrainDataset(train_dir=config.train_dir, style_image=config.style_image, transforms=transform)
@@ -48,7 +66,11 @@ def main(config):
     vgg19.eval()
 
     generator = Generator(image_size=config.image_size).to(device)
+    # generator = Generator().to(device)
     cxloss = CXLoss().to(device)
+
+    if config.load_model:
+        generator.load_state_dict(torch.load(config.load_model))
 
     optimizer = optim.Adam(params=generator.parameters(), lr=config.lr)
 
@@ -58,46 +80,110 @@ def main(config):
     if not exists(config.result_path):
         os.makedirs(config.result_path)
 
-    for epoch in range(1, config.epochs + 1):
+    for epoch in range(1 + config.start_idx, config.epochs + 1):
 
         for i, data in enumerate(train_loader):
-            content = data['content'].to(device).float()
+            source = data['source'].to(device).float()
             style = data['style'].to(device).float()
 
             optimizer.zero_grad()
 
-            _, vgg_content = vgg19(content)
-            vgg_style, _ = vgg19(style)
-            fake = generator(content)
-            fake_style, fake_content = vgg19(fake)
+            source_32, source_42 = vgg19(source)
+            style_32, style_42 = vgg19(style)
+            fake = generator(source)
+            for j in range(len(mean)):
+                fake[:, j, :, :] = (fake[:, j, :, :] - mean[j]) / std[j]
+            fake_32, fake_42 = vgg19(fake)
 
-            cx_style_loss = config.lambda_style * cxloss(vgg_style, fake_style)
-            cx_content_loss = config.lambda_content * cxloss(vgg_content, fake_content)
+            cx_style_loss = config.lambda_style * (cxloss(style_32, fake_32))
+            cx_content_loss = config.lambda_content * cxloss(source_42, fake_42)
 
             loss = cx_style_loss + cx_content_loss
 
             loss.backward()
             optimizer.step()
 
-            if i % 10 == 0:
+            if i % 100 == 0:
                 print("Epoch: %d/%d | Step: %d/%d | Style loss: %f | Content loss: %f | Loss: %f" %
-                      (epoch, config.epochs+1, i, len(train_loader), cx_style_loss.item(), cx_content_loss.item(), loss.item()))
+                      (epoch, config.epochs, i, len(train_loader), cx_style_loss.item(), cx_content_loss.item(), loss.item()))
 
             if (i + 1) % 500 == 0:
                 torch.save(generator.state_dict(), join(config.ckpt_path, 'epoch-%d.pkl' % epoch))
 
-        for i, data in enumerate(test_loader):
-            generator.eval()
-            result_path = join(config.result_path, 'epoch_%d' % epoch)
-            if not exists(result_path):
-                os.makedirs(result_path)
+            if i % 100 == 0:
+                plt.subplot(131)
+                plt.imshow(tensor2image(source))
+                plt.title('source')
+                plt.subplot(132)
+                plt.imshow(tensor2image(style))
+                plt.title('style')
+                plt.subplot(133)
+                plt.imshow(tensor2image(fake))
+                plt.title('fake')
+                plt.tight_layout()
+                plt.savefig(join(config.result_path, 'epoch-%d-step-%d.png' % (epoch, i)))
 
-            content = data['content'].to(device).float()
-            fake = generator(content)
-            fake = (fake.detach().cpu().numpy()[0] * 255).astype(np.uint8)
-            plt.imshow(fake)
+        generator.eval()
+        result_path = join(config.result_path, 'epoch-%d' % epoch)
+        if not exists(result_path):
+            os.makedirs(result_path)
+        for i, data in enumerate(test_loader):
+            source = data['source'].to(device).float()
+            fake = generator(source)
+            plt.subplot(131)
+            plt.imshow(tensor2image(source))
+            plt.title('content')
+            plt.subplot(132)
+            plt.imshow(tensor2image(style))
+            plt.title('style')
+            plt.subplot(133)
+            fake = (fake.detach().cpu().numpy()[0].transpose((1, 2, 0)) * 255).astype(np.uint8)
+            fake = np.minimum(np.maximum(fake, 0.0), 255.0)
+            plt.imshow(fake.astype(np.uint8))
+            plt.title('fake')
             plt.savefig(join(result_path, 'step-%d.png' % (i+1)))
-            generator.train()
+        generator.train()
+
+
+def test(config):
+    print(config)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    normalize = transforms.Normalize(mean=mean, std=std)
+    transform = transforms.Compose([
+        transforms.Resize((config.image_size, config.image_size)),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    test_dataset = TestDataset(test_dir=config.test_dir, transforms=transform)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1)
+
+    generator = Generator(image_size=config.image_size).to(device)
+
+    if config.load_model:
+        generator.load_state_dict(torch.load(config.load_model))
+
+    if not exists(config.result_path):
+        os.makedirs(config.result_path)
+
+    generator.eval()
+    if not exists(config.result_path):
+        os.makedirs(config.result_path)
+    for i, data in enumerate(test_loader):
+        source = data['source'].to(device).float()
+        fake = generator(source)
+        plt.subplot(121)
+        plt.imshow(tensor2image(source))
+        plt.title('source')
+        plt.subplot(122)
+        fake = (fake.detach().cpu().numpy()[0].transpose((1, 2, 0)) * 255).astype(np.uint8)
+        fake = np.minimum(np.maximum(fake, 0.0), 255.0)
+        plt.imshow(fake.astype(np.uint8))
+        plt.title('fake')
+        plt.savefig(join(config.result_path, 'step-%d.png' % (i+1)))
+    generator.train()
 
 
 if __name__ == '__main__':
@@ -115,8 +201,15 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--num_workers', type=int, default=4)
-    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--load_model', type=str, default=None)
+    parser.add_argument('--start_idx', type=int, default=0)
+    # 1 train | 0 test
+    parser.add_argument('--train', type=int, default=1)
 
     config = parser.parse_args()
 
-    main(config)
+    if config.train == 1:
+        main(config)
+    elif config.train == 0:
+        test(config)
